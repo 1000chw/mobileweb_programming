4855f1f7de0ff10d433ad044d13c25f9772828ca
Chun Hyunwoo <1000chw@gmail.com>
2023-12-08 02:18:03 +0900

    Update settings.py

--- myvenv1/mysite/settings.py
+++ myvenv1/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t7!cirb&+=#wll$qmvxtey*$qb8*h_^dp02to!h-$p22_@=nmq'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
